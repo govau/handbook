07028ebdd3daa480c17e4decdcd1ea775db38104
colugo <alastair.parker@gmail.com>
2017-04-13 11:23:56 +1000

    Configure browsersync to mirror deployed path

---
+++ bs-config.js
@@ -0,0 +1,26 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+  "files": ['site/*.html','site/*/*.html', 'site/css/*.css', 'site/js/*.js'],
+  "server": {
+    baseDir: 'app',
+    //Route paths are not relative to the base directory
+    routes: {
+      '/handbook/site/': 'site/',
+    }
+  },
+  "startPath": "/handbook/site/"
+
+};
